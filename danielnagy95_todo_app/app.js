7cea0041f4a8361004fa18cc480affc95451d1e3
danielnagy95 <danielnagy95@icloud.com>
2021-11-27 11:14:18 +0100

    start task

--- danielnagy95_todo_app/app.js
+++ danielnagy95_todo_app/app.js
@@ -1,8 +1,23 @@
-Parancssori Todo applikáció
-=============================
 
-Parancssori argumentumok:
-    -l   Kilistázza a feladatokat
-    -a   Új feladatot ad hozzá
-    -r   Eltávolít egy feladatot
-    -c   Teljesít egy feladatot
+const args = process.argv.slice(2);
+
+console.log(args);
+
+if (args.includes('-l')) {
+    console.log("listing");
+    // File beolvasást
+    // JSON parseolást => rawTodoList
+    // warTodoList => todolist = [ Todo{}, Todo {}, ]
+    // a todoList kilistázása
+} else {
+    console.log(
+        `Parancssori Todo applikáció
+        =============================
+        
+        Parancssori argumentumok:
+            -l   Kilistázza a feladatokat
+            -a   Új feladatot ad hozzá
+            -r   Eltávolít egy feladatot
+            -c   Teljesít egy feladatot`
+    );
+}
